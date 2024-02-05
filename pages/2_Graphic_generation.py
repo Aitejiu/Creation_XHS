c936bb49706cd76f5ab7ee8310e462f4788f38ad
Aitejiu <73458598+Aitejiu@users.noreply.github.com>
2024-02-05 16:35:23 +0800

    Update 2_Graphic_generation.py

--- pages/2_Graphic_generation.py
+++ pages/2_Graphic_generation.py
@@ -1,7 +1,6 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM, GenerationConfig
 import torch
 import streamlit as st
-from modelscope import snapshot_download
 
 # 侧边栏中创建标题和链接
 with st.sidebar:
